168ae53b1a361caac8aadc7e4a270fcbda6a45ee
David Lejeune <dl@qbmt.be>
2017-08-01 16:15:17 +0200

    generating 1 of 2 test failed

--- src/be/qbmt/test/TestCustomer.java
+++ src/be/qbmt/test/TestCustomer.java
@@ -14,13 +14,18 @@ import junit.framework.TestCase;
 public class TestCustomer extends TestCase{
     
     
-    public void testGetCustomerName() {
+    public void testGetCustomerName1() {
         Customer customer1 = new Customer();
         customer1.setFirstName("John");
         customer1.setLastName("Doe");
         assertEquals("John Doe" , customer1.getCustomerName());
     }
-    
+
+    public void testGetCustomerName2() {
+        Customer customer2 = new Customer();
+        customer2.setFirstName("Elvis");
+        customer2.setLastName(Elvis PresleyJohn Doe" , customer2.getCustomerName());
+    }
     
     
 }
