722dea93534abcf6d2ad42f4a40f586b6fc52c36
David Lejeune <dl@qbmt.be>
2017-08-01 14:46:49 +0200

    rename classes 3

--- src/be/qbmt/code/Customer.java
+++ src/be/qbmt/code/Customer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaunittestapp.code;
+package be.qbmt.code;
 
 /**
  *
--- src/be/qbmt/test/TestCustomer.java
+++ src/be/qbmt/test/TestCustomer.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaunittestapp.test;
-import javaunittestapp.code.Customer;
+package be.qbmt.test;
+import be.qbmt.code.Customer;
 import junit.framework.TestCase;
 
 /**
